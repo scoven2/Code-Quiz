79ccf015b7cd53c3c425874358ae31ad86728fbc
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-05-11 23:15:33 -0500

    might have finally done it. rewrote entire code in different text box then pasted it over and relinked everything

--- assets/js/HighScores.js
+++ assets/js/HighScores.js
@@ -1,20 +1,24 @@
-let highScore = document.querySelector("#highScore");
-let clear = document.querySelector("#clear");
-let goBack = document.querySelector("#goBack");
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+
 
 clear.addEventListener("click", function() {
     localStorage.clear();
     location.reload();
 });
 
-let allScores = localStorage.getItem("allScores");
+var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
 
 if (allScores !== null) {
-    for (let i = 0; i < allScores.length; i++) {
-        let createLi = document.createElement("li");
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
         createLi.textContent = allScores[i].initials + " " + allScores[i].score;
         highScore.appendChild(createLi);
+
     }
 }
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,29 @@
 var questions = [{
         title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts"
     },
     {
         title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "3. parentheses"
     },
     {
         title: "Arrays in Javascript can be used to store ____.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
+        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above"
     },
     {
         title: "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-        answer: "quotes"
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        answer: "3. quotes"
     },
     {
         title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-        answer: "console log"
+        choices: ["1. Javascript", "2. terminal / bash", "3. for loops", "4. console log"],
+        answer: "4. console log"
     },
+
 ];
 
 var score = 0;
@@ -35,6 +36,7 @@ var secondsLeft = 76;
 var holdInterval = 0;
 var penalty = 10;
 var ulCreate = document.createElement("ul");
+
 
 timer.addEventListener("click", function() {
     if (holdInterval === 0) {
@@ -51,6 +53,7 @@ timer.addEventListener("click", function() {
     render(questionIndex);
 });
 
+
 function render(questionIndex) {
     questionsDiv.innerHTML = "";
     ulCreate.innerHTML = "";
@@ -60,7 +63,7 @@ function render(questionIndex) {
         questionsDiv.textContent = userQuestion;
     }
     userChoices.forEach(function(newItem) {
-        var listItem = document.createElement("li")
+        var listItem = document.createElement("li");
         listItem.textContent = newItem;
         questionsDiv.appendChild(ulCreate);
         ulCreate.appendChild(listItem);
@@ -142,7 +145,7 @@ function allDone() {
 
         if (initials === null) {
 
-            console.log("No value entered!");
+            console.log("No value entered!");
 
         } else {
             var finalScore = {
@@ -159,7 +162,7 @@ function allDone() {
             allScores.push(finalScore);
             var newScore = JSON.stringify(allScores);
             localStorage.setItem("allScores", newScore);
-            window.location.replace("./HighScores.html");
+            window.location.replace("HighScores.html");
         }
     });
 
